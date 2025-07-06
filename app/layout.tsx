9bd56a7fc1ca11bf94eff6fa6f53e164dfa4f054
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2025-07-06 18:46:29 +0000

    favicon addition

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,37 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Zentime",
-  description: "A simple calendar app built with Next.js and TypeScript to handle bookings and appointments",
+  description:
+    "A simple calendar app built with Next.js and TypeScript to handle bookings and appointments",
+  icons: [
+    { rel: "icon", url: "/favicon.ico" },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-touch-icon.png" },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "192x192",
+      url: "/android-chrome-192x192.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "512x512",
+      url: "/android-chrome-512x512.png",
+    },
+    { rel: "manifest", url: "/site.webmanifest" },
+  ],
 };
 
 export default function RootLayout({
@@ -25,9 +55,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{baseTheme: neobrutalism}}>
+    <ClerkProvider appearance={{ baseTheme: neobrutalism }}>
       <html lang="en">
-        
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased animate-fade`}
         >
